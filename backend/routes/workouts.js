18bffc0b77926786118fe4286c35105eb572dcd0
Charles Noah <noahimperium@gmail.com>
2023-05-29 05:54:34 +0800

    backend routes

---
+++ backend/routes/workouts.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const {createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout} = require('../controllers/workoutController')
+
+
+// GET all workouts
+router.get('/', getWorkouts)
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout )
+
+// DELETE a single workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const workoutRoutes = require('./routes/workouts')
+
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+
+})
+
+// routes
+app.use('/api/workouts',workoutRoutes);
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Connected to MongoDB Server & Server is running in port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => console.log(err))
+
+
+
